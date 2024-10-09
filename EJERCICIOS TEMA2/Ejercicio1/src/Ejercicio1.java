a568adb55c3b03a8c95d3f518dd0bc371715dd35
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 19:41:14 +0200

    Ejercicio1_Tema2

---
+++ EJERCICIOS TEMA2/Ejercicio1/src/Ejercicio1.java
@@ -0,0 +1,16 @@
+public class Ejercicio1 {
+    public static void main(String[] args) throws Exception {
+        int x = 144;
+        int y = 999;
+        int sum = x + y ;
+        int res = y - x;
+        int div = y / x;
+        int mul = x * y;
+        System.out.println("El numero 1 es : " + x);
+        System.out.println("El numero 2 es : " + y);
+        System.out.println("La suma es igual a: " + sum);
+        System.out.println("La resta de numero2 menos numero1 es igual a: " + res);
+        System.out.println("La division de numero2 entre numero1 es igual a:  " + div);
+        System.out.println("La multiplicacion de numero1 por numero2 es igual a: " + mul);
+    }
+}
