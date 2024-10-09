dcd3829537f5e8ddcf667efff34709a56691e6c1
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 19:01:42 +0200

    Ejercicio7 real

---
+++ Ejercicio7_Libro/src/Ejercicio7_Libro.java
@@ -0,0 +1,14 @@
+public class Ejercicio7_Libro {
+    public static void main(String[] args) throws Exception {
+        System.out.println("    \u002A    ");
+        System.out.println("   \u002A \u002A   ");
+        System.out.println("  \u002A   \u002A  ");
+        System.out.println(" \u002A     \u002A  ");
+        System.out.println("\u002A\u002A\u002A\u002A\u002A\u002A\u002A\u002A\u002A  ");
+
+
+
+
+    }
+    }
+
