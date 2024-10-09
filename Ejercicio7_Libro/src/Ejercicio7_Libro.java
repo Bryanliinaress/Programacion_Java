3a553bf1aeafb42800d1084e08cf9e6b94176ac0
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 18:56:30 +0200

    Commit nuevo

--- Ejercicio7_Libro/src/Ejercicio7_Libro.java
+++ Ejercicio7_Libro/src/Ejercicio7_Libro.java
@@ -1,4 +1,4 @@
-public class Ejercicio6_Libro {
+public class Ejercicio7_Libro {
     public static void main(String[] args) throws Exception {
         System.out.println("\u002A       \u002A  ");
         System.out.println(" \u002A     \u002A  ");
