68f685f19cd7360e2bcf8b6f7c2cb272ee4ce757
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 18:48:11 +0200

    Ejercicio5_Libro

---
+++ Ejercicio5_Libro/src/Ejercicio5_Libro.java
@@ -0,0 +1,9 @@
+public class Ejercicio5_Libro {
+    public static void main(String[] args) throws Exception {
+        System.out.println("    \u002A    ");
+        System.out.println("   \u002A\u002A\u002A   ");
+        System.out.println("  \u002A\u002A\u002A\u002A\u002A  ");
+        System.out.println(" \u002A\u002A\u002A\u002A\u002A\u002A\u002A  ");
+        System.out.println("\u002A\u002A\u002A\u002A\u002A\u002A\u002A\u002A\u002A  ");
+    }
+}
