26a7a94474ffeaa323f1b9351590bfb58ba8b64c
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 18:58:17 +0200

    Ejercicio8

--- Ejercicio8_Libro/src/Ejercicio8_Libro.java
+++ Ejercicio8_Libro/src/Ejercicio8_Libro.java
@@ -1,6 +1,6 @@
-public class Ejercicio7_Libro {
+public class Ejercicio8_Libro {
     public static void main(String[] args) throws Exception {
-        System.out.println("\u002A       \u002A  ");
+        System.out.println("\u002A\u002A\u002A\u002A\u002A\u002A\u002A\u002A\u002A  ");
         System.out.println(" \u002A     \u002A  ");
         System.out.println("  \u002A   \u002A  ");
         System.out.println("   \u002A \u002A   ");
