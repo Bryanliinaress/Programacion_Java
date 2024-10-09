c41528bb1c8c4da322fad3104bffa700cb9e3b70
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 18:08:28 +0200

    Ejercicio4_Libro

---
+++ Ejercicio4_Libro/src/Ejercicio4_Libro.java
@@ -0,0 +1,21 @@
+public class Ejercicio4_Libro {
+    public static void main(String[] args) throws Exception {
+        System.out.println("┌─────────┬───────────┬───────────┬──────────┬───────────┐");
+        System.out.println("│  Lunes  │   Martes  │ Miercoles │  Jueves  │  Viernes  │ ");
+        System.out.println("├─────────┼───────────┼───────────┼──────────┼───────────┤");
+        System.out.println("│  EDES   │    DIG    │   LMSGI   │  LMSGI   │   BADAT   │");
+        System.out.println("├─────────┼───────────┼───────────┼──────────┼───────────┤");
+        System.out.println("│  EDES   │   BADAT   │    PROG   │  LMSGI   │   BADAT   │");
+        System.out.println("├─────────┼───────────┼───────────┼──────────┼───────────┤");
+        System.out.println("│  EDES   │   BADAT   │    PROG   │   SINF   │    SOS    │");
+        System.out.println("├─────────┴───────────┴───────────┴──────────┴───────────┤");
+        System.out.println("│                         RECREO                         │");
+        System.out.println("├─────────┬───────────┬───────────┬──────────┬───────────┤");
+        System.out.println("│  PROG   │    IPEI   │    PROG   │   SINF   │    SINF   │");
+        System.out.println("├─────────┼───────────┼───────────┼──────────┼───────────┤");
+        System.out.println("│  PROG   │    IPEI   │   BADAT   │   PROG   │    SINF   │");
+        System.out.println("├─────────┼───────────┼───────────┼──────────┼───────────┤");
+        System.out.println("│  PROG   │    IPEI   │   BADAT   │   PROG   │    SINF   │");
+        System.out.println("└─────────┴───────────┴───────────┴──────────┴───────────┘");
+    }
+}
