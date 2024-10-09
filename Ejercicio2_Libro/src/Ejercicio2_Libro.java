c9e38b55e6dfa3bfdbaf33ae02d0d5de6bdda058
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 17:39:23 +0200

    Ejercicio2_Libro

---
+++ Ejercicio2_Libro/src/Ejercicio2_Libro.java
@@ -0,0 +1,7 @@
+public class Ejercicio2_Libro {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bryan Linares Gonzalez");
+        System.out.println("C/Tortola Nº3");
+        System.out.println("673 80 16 02");
+    }
+}
