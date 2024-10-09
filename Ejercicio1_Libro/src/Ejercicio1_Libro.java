fae36c331aa0b43cfb697b7b7b1ebbe2335b15a0
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 17:36:14 +0200

    Ejercicio1_Libro

--- Ejercicio1_Libro/src/Ejercicio1_Libro.java
+++ Ejercicio1_Libro/src/Ejercicio1_Libro.java
@@ -1,5 +1,5 @@
 public class Ejercicio1_Libro {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Bryan Linares Gonzalez");
     }
 }
