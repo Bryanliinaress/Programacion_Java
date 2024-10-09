b606c0f35952382ea905798412227de095cf6273
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-09 18:53:29 +0200

    ejer

--- Ejercicio5_Libro/src/Ejercicio6_Libro.java
+++ Ejercicio5_Libro/src/Ejercicio6_Libro.java
@@ -1,4 +1,4 @@
-public class Ejercicio5_Libro {
+public class Ejercicio6_Libro {
     public static void main(String[] args) throws Exception {
         System.out.println("    \u002A    ");
         System.out.println("   \u002A\u002A\u002A   ");
