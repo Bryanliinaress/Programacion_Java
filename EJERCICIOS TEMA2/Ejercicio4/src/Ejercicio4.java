0624c1ac7faee88c5f8274109da083421dff8f92
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-10 20:43:43 +0200

    ejercicio4

---
+++ EJERCICIOS TEMA2/Ejercicio4/src/Ejercicio4.java
@@ -0,0 +1,9 @@
+public class Ejercicio4 {
+    public static void main(String[] args) throws Exception {
+        double euros= 150;
+        double pesetas;
+        pesetas= euros * 166.386;
+        System.out.printf(+ euros + " Euros equivalan a %8.2f",pesetas);
+
+    }
+}
