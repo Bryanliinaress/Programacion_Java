c84e68c9d62ad9536884596ea14f69a34c00d265
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-10 20:13:57 +0200

    Ejers 2 y 3

---
+++ EJERCICIOS TEMA2/Ejercicio2/src/Ejercicio2.java
@@ -0,0 +1,6 @@
+public class Ejercicio2 {
+    public static void main(String[] args) throws Exception {
+        String nombre= "Bryan Linares Gonzalez";
+        System.out.println(nombre);
+    }
+}
---
+++ EJERCICIOS TEMA2/Ejercicio3/src/Ejercicio3.java
@@ -0,0 +1,11 @@
+public class Ejercicio3 {
+    public static void main(String[] args) throws Exception {
+        String Nombre="Bryan Linares Gonzalez";
+        String Direccion= "C/Tortola Nº3";
+        int Telefono= 673801602;
+        System.out.println(Nombre);
+        System.out.println(Direccion);
+        System.out.println(Telefono);
+    
+    }
+}
