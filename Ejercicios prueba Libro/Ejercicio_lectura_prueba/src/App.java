b2308937fa196c137685e0b08e62dc2700abfda6
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-14 17:19:16 +0200

    Prueba lectura texto

---
+++ Ejercicios prueba Libro/Ejercicio_lectura_prueba/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String Nombre;
+        System.out.print("Dime tu nombre por favor: ");
+        Nombre= System.console().readLine();
+        System.out.println("Hola "+Nombre+" encantado de conocerte. ");
+
+    }
+}
