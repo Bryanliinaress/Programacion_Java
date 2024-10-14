e02e818bb5cf450f432a8a252addf8019038a7ee
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-14 16:59:38 +0200

    Ejercicio7

---
+++ EJERCICIOS TEMA2/Ejercicio 7/src/Ejercicio7.java
@@ -0,0 +1,13 @@
+public class Ejercicio7 {
+    public static void main(String[] args) throws Exception {
+        char letra_a= 'a';
+        char letra_b= 'b';
+        char letra_c= 'c';
+        char letra_d= 'd';
+        String palabraMenta= "Menta";
+        String palabraCocacola="Cocacola";
+        System.out.println(palabraMenta + " "+ palabraCocacola +" "+letra_a+" " +letra_b+" "+ letra_c+" "+ letra_d);
+        System.out.println(palabraCocacola+ palabraMenta);
+        //No es posible ya que un print un solo print no deja. 
+    }
+}
