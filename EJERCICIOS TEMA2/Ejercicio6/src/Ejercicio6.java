f1e0e5579601910fb3e16f2aa229ced042a00f4e
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-14 16:30:35 +0200

    Ejercicio6 Completo

--- EJERCICIOS TEMA2/Ejercicio6/src/Ejercicio6.java
+++ EJERCICIOS TEMA2/Ejercicio6/src/Ejercicio6.java
@@ -4,6 +4,6 @@ public class Ejercicio6 {
         double PrecioConIva;
         double iva=0.21; 
         PrecioConIva=precioNoIva+(precioNoIva*iva);
-        System.out.println("El precio de la luz es igual "+precioNoIva + " sin iva. Y con iva seria igual a "+PrecioConIva+"Teniendo en cuenta que el iva es del 21%" );
+        System.out.println("El precio de la luz es igual "+precioNoIva + " sin iva. Y con iva seria igual a "+PrecioConIva+" Teniendo en cuenta que el iva es del 21%" );
     }
 }
