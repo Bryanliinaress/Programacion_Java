dce383e0f4e4d445a91a1891a52cd256178b168b
Bryanliinaress <blingon0901@g.educaand.es>
2024-10-14 17:12:08 +0200

    Ejercicio8

---
+++ EJERCICIOS TEMA2/Ejercicio8/src/Ejercicio8.java
@@ -0,0 +1,11 @@
+public class Ejercicio8 {
+    public static void main(String[] args) throws Exception {
+        char letra_c= 'c';
+        char letra_l= 'l';
+        char letra_a= 'a';
+        char letra_s= 's';
+        char letra_e= 'e';
+        String palabraClase=( Character.toString(letra_c)+letra_l+letra_a+letra_s+letra_e) ;
+        System.out.println(palabraClase);
+    }
+}
