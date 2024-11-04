04fd507ef8d7e873e85e1784c49e6cc7ec2c2f90
Bryanliinaress <blingon0901@g.educaand.es>
2024-11-04 19:31:55 +0100

    Ejercicio1_Tema5

---
+++ EJERCICIOS TEMA5/Ejercicio1_Tema5/src/Ejercicio1_Tema5.java
@@ -0,0 +1,9 @@
+public class Ejercicio1_Tema5 {
+    public static void main(String[] args) throws Exception {
+        int multiplosCinco=0;
+        for(int i=1; i<21; i++){
+            multiplosCinco+=5;
+            System.out.println("5x"+i+"="+multiplosCinco);
+        }
+    }
+}
