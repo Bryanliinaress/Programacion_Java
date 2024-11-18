9f000fd91e6df4584bc3d890fb802fa2bf0ec2ec
Bryan Linares Gonzalez <blingon0901@g.educaand.es>
2024-11-18 17:46:37 +0100

    Primer uso de Mockito

--- EDES+Java/Prueba_Paso2/src/ServiceTest.java
+++ EDES+Java/Prueba_Paso2/src/ServiceTest.java
@@ -1,10 +1,20 @@
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.junit.Assert;
 
 public class ServiceTest {
-    @Test
-    public void test() {
-        Service service = new Service();
+        @Test
+        public void test() {
+            Service service = new Service();
+            Assert.assertEquals("Hello", service.says());
+        }
+        @Test
+        public void test2(){
+            Service service = mock(Service.class);
+        when(service.says()).thenReturn("Hello");
         Assert.assertEquals("Hello", service.says());
     }
 }
