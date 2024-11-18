972e9cac77e180fc682fc0c3202540eed3fb2e2d
Bryan Linares Gonzalez <blingon0901@g.educaand.es>
2024-11-18 19:03:00 +0100

    TerminarMockitoEjer1

---
+++ EDES+Java/Ejercicios_JUnit/Ejercicios_Mockito/Ejercicio1/src/RepositorioUsuarios.java
@@ -0,0 +1,9 @@
+public class RepositorioUsuarios {
+    public String usuarioExistente() {
+        return "Existe";
+    }
+
+    public String usuarioNoExistente() {
+        return "No Existe";
+    }
+}
