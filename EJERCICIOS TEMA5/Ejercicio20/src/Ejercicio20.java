c9e5f7094d16d18f2575f4c5cdf53194801431e1
Bryan Linares Gonzalez <blingon0901@g.educaand.es>
2024-11-20 19:35:43 +0100

    Ejercicio26

--- EJERCICIOS TEMA5/Ejercicio20/src/Ejercicio20.java
+++ EJERCICIOS TEMA5/Ejercicio20/src/Ejercicio20.java
@@ -2,34 +2,34 @@ import java.util.Scanner;
 
 public class Ejercicio20 {
     public static void main(String[] args) throws Exception {
-        Scanner s = new Scanner(System.in);
-
-        System.out.print("Dime de que altura quiere que sea tu piramide hueca: ");
-        int alturaPiramide = s.nextInt();
-        System.out.print("Dime de que caracter quieres que este formada tu piramide: ");
-        s.nextLine();
-        String simboloPiramide = s.nextLine();
-        int base=1;
-        int espacioPordelante= alturaPiramide-1;
-        int espacioInterior=0;
-        for ( base=1;  base<= alturaPiramide; base++){
-            for (int i = 0 ; i<= espacioPordelante; i++){
-                System.out.print(" ");
-            }
-            System.out.print(simboloPiramide);
-            for(int i=2 ; i<= espacioInterior; i++){
-                System.out.print(" ");
-            }
-            if(base>1){
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.print("Dime de que altura quiere que sea tu piramide hueca: ");
+            int alturaPiramide = s.nextInt();
+            System.out.print("Dime de que caracter quieres que este formada tu piramide: ");
+            s.nextLine();
+            String simboloPiramide = s.nextLine();
+            int base=1;
+            int espacioPordelante= alturaPiramide-1;
+            int espacioInterior=0;
+            for ( base=1;  base<= alturaPiramide; base++){
+                for (int i = 0 ; i<= espacioPordelante; i++){
+                    System.out.print(" ");
+                }
                 System.out.print(simboloPiramide);
+                for(int i=2 ; i<= espacioInterior; i++){
+                    System.out.print(" ");
+                }
+                if(base>1){
+                    System.out.print(simboloPiramide);
+                }
+                System.out.println();
+                espacioInterior+=2;
+                espacioPordelante--;
             }
-            System.out.println();
-            espacioInterior+=2;
-            espacioPordelante--;
+            for (int i =2 ; i <=base*2; i++){
+            System.out.print(simboloPiramide);
+            }
+            s.close();
         }
-        for (int i =2 ; i <=base*2; i++){
-        System.out.print(simboloPiramide);
-        }
-        s.close();
     }
 }
---
+++ EJERCICIOS TEMA5/Ejercicio26/src/Ejercicio26.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio26 {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Dame un numero de minimo 10 digitos: ");
+        int numero = s.nextInt();
+        System.out.println("Introduce un digito: ");
+        int digitoIntroducido = s.nextInt();
+        if (numero < 10) {
+            System.out.println("El numero ingresado no tiene 10 digitos");
+        }else{
+            for (int i =1 ; numero != 0; i++ ) {
+                int digito = numero % 10;
+                if (digito==digitoIntroducido) {
+                    System.out.println("El "+digitoIntroducido+" se encuentra en la posicion "+(i+1));
+                }
+            }
+        }    
+        s.close();
+    }
+}
