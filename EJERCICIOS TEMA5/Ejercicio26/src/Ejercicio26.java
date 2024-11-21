26a65e58817c0aa6e36627db60ec4ea025596dbb
Bryanliinaress <blingon0901@g.educaand.es>
2024-11-22 00:03:57 +0100

    Ejercicio Examen4completo

--- EJERCICIOS TEMA5/Ejercicio26/src/Ejercicio26.java
+++ EJERCICIOS TEMA5/Ejercicio26/src/Ejercicio26.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 public class Ejercicio26 {
     public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
-        System.out.println("Dame un numero de minimo 10 digitos: ");
-        int numero = s.nextInt();
-        System.out.println("Introduce un digito: ");
+        System.out.print("Dame un numero de minimo 10 digitos: ");
+        long numero = s.nextInt();
+        System.out.print("Introduce un digito: ");
         int digitoIntroducido = s.nextInt();
+        int i=0;
         if (numero < 10) {
             System.out.println("El numero ingresado no tiene 10 digitos");
         }else{
-            for (int i =1 ; numero != 0; i++ ) {
-                int digito = numero % 10;
-                if (digito==digitoIntroducido) {
-                    System.out.println("El "+digitoIntroducido+" se encuentra en la posicion "+(i+1));
+            long numeroReverso=0;
+            for ( i =1 ; numero != 0; i++ ) {
+                long digito = numero % 10;
+                numeroReverso = (numeroReverso * 10) + digito;
+                numero = numero /10;
+
+            }
+            for ( i = 1 ; numeroReverso != 0; i++){
+                long digitoReverso = numeroReverso % 10;
+                if (digitoReverso==digitoIntroducido){
+                    System.out.println("Tu digito " + digitoIntroducido+ " se encuentra en la posicion " +i);
                 }
+                numeroReverso= numeroReverso /10;
             }
         }    
         s.close();
---
+++ EXAMEN4/Ejercicio_Examen4.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Ejercicio_Examen4 {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        int intentosContraseña = 3;
+        String contraseñaUsuario;
+        int contraseñaValida = 1;
+        while (contraseñaValida <= intentosContraseña) {
+            System.out.print("Por favor, introduzca una contraseña segura: ");
+            contraseñaUsuario = s.nextLine();
+            if (caracteresMinimos(contraseñaUsuario)) {
+                if (Mayuscula(contraseñaUsuario)) {
+                    if (Minuscula(contraseñaUsuario)) {
+                        if (contieneNumero(contraseñaUsuario)) {
+                            if (caracterEspecial(contraseñaUsuario)) {
+                                System.out.println("\033[0;92mContraseña segura.\033[0m");
+                                contraseñaValida =5;
+                            } else {
+                                System.out.println(
+                                        "\033[0;91mError: La contraseña debe contener al menos un caracter especial.\033[0m");
+                            }
+                        } else {
+                            System.out
+                                    .println("\033[0;91mError: La contraseña debe contener al menos un dígito.\033[0m");
+                        }
+                    } else {
+                        System.out.println(
+                                "\033[0;91mError: La contraseña debe contener al menos un carácter minuscula.\033[0m");
+                    }
+                } else {
+                    System.out.println(
+                            "\033[0;91mError: La contraseña debe contener al menos un carácter en mayuscula.\033[0m");
+                }
+            } else {
+                System.out.println("\033[0;91mError: La contraseña debe contener al menos 8 caracteres\033[0m");
+            }
+            contraseñaValida++;
+        }
+        if (contraseñaValida == 4) {
+            System.out.println("\033[0;93mNúmero máximo de intentos alcanzado.\033[0m");
+        }
+        s.close();
+    }
+
+    public static boolean caracteresMinimos(String contraseñaMin) {
+        char letrasMini = 1;
+        for (int i = 0; i < contraseñaMin.length(); i++) {
+            letrasMini++;
+        }
+        if (letrasMini >= 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean Mayuscula(String contraseñaMayuscula) {
+        char letraMayuscula = 0;
+        char letra;
+        for (int i = 0; i < contraseñaMayuscula.length(); i++) {
+            letra = contraseñaMayuscula.charAt(i);
+            if (Character.isUpperCase(letra)) {
+                letraMayuscula = letra;
+            }
+        }
+        if (letraMayuscula != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean Minuscula(String contraseñaMinuscula) {
+        char letraMinuscula = 0;
+        char letra;
+        for (int i = 0; i < contraseñaMinuscula.length(); i++) {
+            letra = contraseñaMinuscula.charAt(i);
+            if (Character.isLowerCase(letra)) {
+                letraMinuscula = letra;
+            }
+        }
+        if (letraMinuscula != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean contieneNumero(String contraseñaNumero) {
+        char letraNumero = 0;
+        char letra;
+        for (int i = 0; i < contraseñaNumero.length(); i++) {
+            letra = contraseñaNumero.charAt(i);
+            if (Character.isDigit(letra)) {
+                letraNumero = letra;
+            }
+        }
+        if (letraNumero != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean caracterEspecial(String contraseñaCaracterEspecial) {
+        String caracteresEspeciales = "!@#$%^&*()-_+=<>?/.,;:";
+        char letraCaracterEspecial = 0;
+        char letra;
+        for (int i = 0; i < contraseñaCaracterEspecial.length(); i++){
+            letra = contraseñaCaracterEspecial.charAt(i);
+            for (int j = 0; j < caracteresEspeciales.length(); j++){
+                if (letra == caracteresEspeciales.charAt(j)){
+                    letraCaracterEspecial = letra;
+                }
+            }
+        }
+        if (letraCaracterEspecial != 0) {
+        return true;
+        } else{
+            return  false;
+        }
+    }
+}
+
