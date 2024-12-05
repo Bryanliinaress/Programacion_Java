d1727a41a3659d8c32fc3f0230e95d459d975945
Bryan Linares Gonzalez <blingon0901@g.educaand.es>
2024-12-05 17:26:16 +0100

    prueba

--- EjercicioPruebaExamen6/Ejercicio/src/org/alangturing/texto/org/alangturing/texto/CadenasTexto.java
+++ EjercicioPruebaExamen6/Ejercicio/src/org/alangturing/texto/org/alangturing/texto/CadenasTexto.java
@@ -1,5 +1,7 @@
 package org.alangturing.texto;
 
+import matematicas.*;
+import java.util.Scanner;
 
 public class CadenasTexto {
     /*
@@ -20,15 +22,14 @@ public class CadenasTexto {
         return contador;
     }
 
-
     /**
      * 
      * @param frase se le va a quitar los espacios
      * @return la frase sin espacios
      */
-    public static String sinEspacios (String frase){
-        String nuevaFrase="";
-        String espacioEnBlanco=" ";
+    public static String sinEspacios(String frase) {
+        String nuevaFrase = "";
+        String espacioEnBlanco = " ";
         for (int i = 0; i < frase.length(); i++) {
             if (frase.charAt(i) != espacioEnBlanco.charAt(0)) {
                 nuevaFrase += frase.charAt(i);
@@ -36,6 +37,7 @@ public class CadenasTexto {
         }
         return nuevaFrase;
     }
+
     /**
      * 
      * @param frase la cual se le va a dar la vuelta
@@ -43,50 +45,71 @@ public class CadenasTexto {
      * @return devuelve la frase dada la vuelta
      */
     public static String invertirPalabra(String frase) {
-        String nuevaFrase= CadenasTexto.sinEspacios(frase);
-        int longitudFrase = nuevaFrase.length()-1;
-        String nuevaFraseInversa="";
-        for (int i = longitudFrase; i>= 0; i--){
-            nuevaFraseInversa+= nuevaFrase.charAt(i);
+        String nuevaFrase = CadenasTexto.sinEspacios(frase);
+        int longitudFrase = nuevaFrase.length() - 1;
+        String nuevaFraseInversa = "";
+        for (int i = longitudFrase; i >= 0; i--) {
+            nuevaFraseInversa += nuevaFrase.charAt(i);
         }
         return nuevaFraseInversa;
     }
+
     /**
      * 
      * @param frase1 la vamos a usar para comparar con frase2
-     * @param frase2 la comparamos con frase1 
-     * @return esperamos que frase1 y frase2 sean iguales estando una invertida respecto a la otra
+     * @param frase2 la comparamos con frase1
+     * @return esperamos que frase1 y frase2 sean iguales estando una invertida
+     *         respecto a la otra
      */
-    public static boolean esPalindromas (String frase1, String frase2){
+    public static boolean esPalindromas(String frase1, String frase2) {
         String fraseInvertida = CadenasTexto.invertirPalabra(frase2);
-        String fraseNormal= CadenasTexto.sinEspacios(frase1);
+        String fraseNormal = CadenasTexto.sinEspacios(frase1);
         if (fraseNormal.equals(fraseInvertida)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     public static void main(String[] args) {
         String pruebaFrase = "Amor a Roma";
-        String fraseInvertida= CadenasTexto.invertirPalabra(pruebaFrase);
-        String pruebaFraseMinuscula= "";
-        String fraseInvertidaMinuscula="";
-        if (CadenasTexto.contadorVocales(pruebaFrase)== CadenasTexto.contadorVocales(fraseInvertida)) {
+        String fraseInvertida = CadenasTexto.invertirPalabra(pruebaFrase);
+        String pruebaFraseMinuscula = "";
+        String fraseInvertidaMinuscula = "";
+        if (CadenasTexto.contadorVocales(pruebaFrase) == CadenasTexto.contadorVocales(fraseInvertida)) {
             System.out.println("Contiene las mismas vocales.");
-            for (int i = 0; i<pruebaFrase.length() ; i++){
+            for (int i = 0; i < pruebaFrase.length(); i++) {
                 pruebaFraseMinuscula += Character.toLowerCase(pruebaFrase.charAt(i));
             }
-            for (int i = 0; i<fraseInvertida.length(); i++){
+            for (int i = 0; i < fraseInvertida.length(); i++) {
                 fraseInvertidaMinuscula += Character.toLowerCase(fraseInvertida.charAt(i));
             }
             if (esPalindromas(pruebaFraseMinuscula, fraseInvertidaMinuscula)) {
                 System.out.println("Y si es Palindroma");
-            
-            }else
-            System.out.println("Pero no es palindroma");
-        }else 
+
+            } else
+                System.out.println("Pero no es palindroma");
+        } else
             System.out.println("no contiene los mismos caracteres por lo tanto no pueden ser palindromas");
-        
+        Scanner s = new Scanner(System.in);
+
+        int numero;
+        System.out.println("dame numero");
+        numero = s.nextInt();
+        int numeroInvertido = Ejercicio1_14.voltearNumeros(numero);
+        int numeroEnSigular = numeroInvertido % 10;;
+        while (numeroInvertido > 10) {
+            numeroInvertido = numeroInvertido / 10;
+            for (int i = 1; i <= numeroEnSigular; i++) {
+                System.out.print("|");
+            }
+            System.out.print(" - ");
+            numeroEnSigular = numeroInvertido % 10;
+        }
+            for (int i = 1; i <= numeroEnSigular; i++) {
+                System.out.print("|");
+            }
+        s.close();
     }
 }
+             
