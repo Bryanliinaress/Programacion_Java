215e2301da4d25ab4fe2b8a2031db59fc785e496
Bryan Linares Gonzalez <blingon0901@g.educaand.es>
2025-01-15 17:02:30 +0100

    Ejercicio13_TEma7

---
+++ EJERCICIOS TEMA7/Ejercicio 13/src/Ejercicio13_Tema7.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Ejercicio13_Tema7 {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        int maximo = 0;
+        int minimo = 100;
+        int comprobador1 = 0;
+        int comprobador2 = 0;
+        int[] arr = new int[100];
+        int eleccionPersona = 0;
+        for (int i = 0; i < 100; i++) {
+            arr[i] = (int) (Math.random() * 501);
+
+            if (arr[i] < minimo) {
+                minimo = arr[i];
+                comprobador1 = i;
+            }
+
+            if (arr[i] > maximo) {
+                maximo = arr[i];
+                comprobador2 = i;
+            }
+        }
+
+        for (int a: arr) {
+            System.out.print(a + "  ");
+        }
+        System.out.println();
+        System.out.print("¿Qué quieres destacar? (1-mínimo, 2-máximo)");
+        eleccionPersona = s.nextInt();
+        System.out.println();
+        switch (eleccionPersona) {
+            case 1:
+                for (int i = 0; i < 100; i++) {
+                    if (i != comprobador1) {
+                        System.out.print(arr[i] + "  ");
+                    } else {
+                        System.out.print("**" + arr[i] + "**  ");
+                    }
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < 100; i++) {
+                    if (i != comprobador2) {
+                        System.out.print(arr[i] + "  ");
+                    } else {
+                        System.out.print("**" + arr[i] + "**  ");
+                    }
+                }
+                break;
+
+            default:
+                System.out.println("ERROR");
+                break;
+
+        }
+        s.close();
+    }
+}
