b9a38dfae5aa643acf70bb8a6a69e17f95d2fbae
Bryan <blingon0901@g.educaand.es>
2025-01-20 23:20:36 +0100

    Delete Ejercicios prueba Libro directory

--- Ejercicios prueba Libro/Ejercicio_lectura_prueba/src/EjerLectura.java
+++
@@ -1,9 +0,0 @@
-public class EjerLectura {
-    public static void main(String[] args) throws Exception {
-        String Nombre;
-        System.out.print("Dime tu nombre por favor: ");
-        Nombre= System.console().readLine();
-        System.out.println("Hola "+Nombre+" encantado de conocerte. ");
-
-    }
-}
